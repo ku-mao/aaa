b922295c42e807b198a36315fbd1372b49e80db2
dairongrong <1335446763@qq.com>
2019-09-23 12:20:39 +0800

    第三次提交

--- test_git/src/edu/xalead/HelloWorld.java
+++ test_git/src/edu/xalead/HelloWorld.java
@@ -6,4 +6,7 @@ public class HelloWorld {
         System.out.println("Hello world !");
         System.out.println("你好");
     }
+    public void test(){
+        System.out.println("en");
+    }
 }
